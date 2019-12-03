92e530510503024eaeccc0d4a1cee38219a104c0
JacksonBellinger <jdunnbellinger@gmail.com>
2019-12-02 20:37:16 -0500

    task2

---
+++ lab10/task2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/types.h>
+
+int count = 0;
+pthread_t thread_id[2];
+
+void* producer()
+{
+	while(1)
+	{
+		if(count == 10)
+		{
+			puts("prod pause");
+			pause();
+		}
+		count ++;
+		if(count == 1)
+			pthread_kill(thread_id[1], SIGCONT);
+		printf("producer count: %d\n", count);
+
+	}
+}
+
+void* consumer()
+{
+	while(1)
+	{
+		if(count == 0)
+		{
+			puts("cons pause");
+			pause();
+		}
+		count --;
+		if(count == 10)
+			pthread_kill(thread_id[0], SIGCONT);
+		printf("consumer count: %d\n", count);
+	}
+}
+
+int main(int argc, char** argv)
+{
+
+
+	pthread_create(&thread_id[0], NULL, producer, NULL);
+	pthread_create(&thread_id[1], NULL, consumer, NULL);
+
+	pthread_join(thread_id[0], NULL);
+	pthread_join(thread_id[1], NULL);
+
+	exit(0);
+}
